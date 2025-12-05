8ef44c06ddd43120bd2bdc02c6d77e3823b2c91a
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-12-04 22:26:11 -0500

    feat(footer): add new social media links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,9 @@ export default function Footer() {
   return (
     <footer id="contact" className="px-6 py-20 max-w-3xl mx-auto text-gray-600">
       <h2 className="text-2xl font-bold mb-4">Contact</h2>
-      <p>Correo: <a href="mailto:tuemail@email.com" className="text-blue-600">tuemail</a></p>
-      <p>GitHub: <a href="https://github.com/tuusuario" className="text-blue-600">tuusuario</a></p>
+      <p>Correo: <a href="mailto:velasqueznikol10@gmail.com" className="text-blue-600">velasquenikol10@gamil.com</a></p>
+      <p>GitHub: <a href="https://github.com/Nikotastic" className="text-blue-600">Nikotastic</a></p>
+      <p>Linkedin: <a href="https://www.linkedin.com/in/nikol-velasquez/" className="text-blue-600">Nikol Velasquez Ramos</a></p>
     </footer>
-  );
+  );  
 }
